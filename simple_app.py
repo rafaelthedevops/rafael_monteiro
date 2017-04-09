a18948c79b74ffd932aec6ca793e72f8c5aab82e
Rafael Monteiro <rafaelmonteiroti@gmail.com>
2017-04-09 01:30:28 -0300

    set to listen on port 3000 and return json instead of plain text

--- simple_app.py
+++ simple_app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/<anystring>')
 def show_anystring(anystring):
-    return anystring
+    return jsonify(anystring)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=3000)
